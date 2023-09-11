55c6159d7b68714b423550dea2e6490bb73ba881
TavonnS <142205891+TavonnS@users.noreply.github.com>
2023-09-10 21:15:19 -0700

    final project commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Password Generator Starter Code
+# Password Generator Application
+
+## Description
+This is a password generator web app that will ask a few questions to construct a password, then display the password in the main text area.  It was a homework project designed to apply the JavaScript we have learned to date.  It uses concatenation, a for loop, and randomization, among other JavaScript codes. (Note that for this app to run correctly, it must be reloaded between password generations.)
+
+## Screenshot
+![Screenshot](PassGenApp-screenshot.png)
+
+## Credits
+I did not write the source code for this project.  It may be found here: [Link to soure code on GitHub](https://github.com/coding-boot-camp/friendly-parakeet)
+
+## Link
+[Link to deployed app](https://tavonns.github.io/password-generator-app)
+
+## License
+MIT License
--- script.js
+++ script.js
@@ -1,16 +1,70 @@
-// Assignment code here
+
+var generateBtn = document.getElementById("generate")
+
+var includeUppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+var includeLowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  
+var includeNumeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  
+var includeSpecial = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',' , ')', '(', '}', '{', ']', '[','~', '-', '_', '.',]
+
+var totalCharacters = []
+ 
+function generatePassword (){
+var passLength = prompt("What is the required length of your password?")
+
+if (passLength < 8 || passLength > 128) {
+  alert("Please enter a length greater than 7 and less than 128")
+  return 
+}
+
+var uppers = confirm("Do you need uppercase characters included?")
+
+if (uppers) {
+  totalCharacters=totalCharacters.concat(includeUppercase)
+}
+
+var lowers = confirm("Do you need lowercase characters included?")
+
+if (lowers) {
+  totalCharacters=totalCharacters.concat(includeLowercase)
+}
+
+var numeric = confirm("Do you need numeric characters included?")
+
+if (numeric) {
+  totalCharacters=totalCharacters.concat(includeNumeric)
+}
+
+var specials = confirm("Do you need special characters included?")
+
+if (specials) {
+  totalCharacters=totalCharacters.concat(includeSpecial)
+}
+console.log(totalCharacters);
+var password = []
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+for (let i=0; i < passLength; i++) {
+  var randomIndex = Math.floor(Math.random()*totalCharacters.length)
+  var randomChar = totalCharacters[randomIndex]
+  password=password.concat(randomChar)
+  
+}
+
+return password.join("");
+
+}
+
+
 
 // Write password to the #password input
-function writePassword() {
+
+function writePassword( ) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
